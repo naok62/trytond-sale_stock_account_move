05ca844b2a33b495574953f3f8e1592fc37bba30
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-07-21 15:40:56 +0200

    On tests create a default account for pending invoices

--- tests/test_sale_stock_account_move.py
+++ tests/test_sale_stock_account_move.py
@@ -5,6 +5,8 @@ import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
+from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.transaction import Transaction
 
 
 class TestCase(unittest.TestCase):
@@ -12,6 +14,9 @@ class TestCase(unittest.TestCase):
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_stock_account_move')
+        self.company = POOL.get('company.company')
+        self.config = POOL.get('sale.configuration')
+        self.account = POOL.get('account.account')
 
     def test0005views(self):
         '''
@@ -24,6 +29,25 @@ class TestCase(unittest.TestCase):
         Test depends.
         '''
         test_depends()
+
+    def test0010_set_default_pending_account(self):
+        with Transaction().start(DB_NAME, USER,
+                context=CONTEXT) as transaction:
+            #This is needed in order to get default values for other test
+            #executing in the same database
+            company, = self.company.search([
+                    ('rec_name', '=', 'Dunder Mifflin'),
+                    ])
+            accounts = self.account.search([
+                    ('kind', '=', 'receivable'),
+                    ], limit=1)
+            if accounts:
+                account, = accounts
+                with transaction.set_context(company=company.id):
+                    config = self.config(1)
+                    config.pending_invoice_account = account
+                    config.save()
+                transaction.cursor.commit()
 
 
 def suite():
