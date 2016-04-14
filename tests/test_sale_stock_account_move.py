e858896da33b40744cd41f14d63e5cd9bb11f308
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-14 10:36:53 +0200

    Migrate to version 3.8 (grafted from ebd0c53118c795756d0b42e9565b7ff15e289e1c)

--- tests/test_sale_stock_account_move.py
+++ tests/test_sale_stock_account_move.py
@@ -4,59 +4,17 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_stock_account_move')
-        self.company = POOL.get('company.company')
-        self.config = POOL.get('sale.configuration')
-        self.account = POOL.get('account.account')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('sale_stock_account_move')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
-
-    def test0010_set_default_pending_account(self):
-        with Transaction().start(DB_NAME, USER,
-                context=CONTEXT) as transaction:
-            #This is needed in order to get default values for other test
-            #executing in the same database
-            company, = self.company.search([
-                    ('rec_name', '=', 'Dunder Mifflin'),
-                    ])
-            accounts = self.account.search([
-                    ('kind', '=', 'receivable'),
-                    ], limit=1)
-            if accounts:
-                account, = accounts
-                with transaction.set_context(company=company.id):
-                    config = self.config(1)
-                    config.pending_invoice_account = account
-                    config.save()
-                transaction.cursor.commit()
+    module = 'sale_stock_account_move'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.company.tests import test_company
-    for test in test_company.suite():
-        if test not in suite:
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_sale.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
